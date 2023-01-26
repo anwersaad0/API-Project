0922ac8ac1896ce1b8dc0c93d8fefcbf7d43b853
anwersaad0 <anwer.saad0@gmail.com>
2023-01-26 00:04:47 -0800

    Added post image endpoint to review route

--- authenticate-me/backend/routes/api/reviews.js
+++ authenticate-me/backend/routes/api/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { Review, User, Spot } = require('../../db/models');
+const { Review, User, Spot, ReviewImage } = require('../../db/models');
 
 const router = express.Router();
 
@@ -14,6 +14,17 @@ router.get('/current', restoreUser, requireAuth, async (req, res) => {
 
     res.status(200);
     res.json(revsByUser);
+});
+
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+    const {url} = req.body;
+    const reviewImage = await ReviewImage.create({
+        reviewId: req.params.reviewId,
+        url
+    });
+
+    res.status(200);
+    res.json(reviewImage);
 });
 
 router.put('/:reviewId', requireAuth, async (req, res) => {
