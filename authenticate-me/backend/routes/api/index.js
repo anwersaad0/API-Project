f86e7f5018eccce84dc713957d667278cd0a0679
anwersaad0 <anwer.saad0@gmail.com>
2023-02-09 22:16:49 -0800

    Frontend Setup Done

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -25,9 +25,11 @@ router.use('/spot-images', spotImagesRouter);
 
 router.use('/review-images', reviewImagesRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+/*
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
+*/
 
 /*
 // GET /api/set-token-cookie
