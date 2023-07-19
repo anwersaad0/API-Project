1cf70851d870d4a37a5711df7cd0fb26be2f86a3
anwersaad0 <anwer.saad0@gmail.com>
2023-07-19 11:38:26 -0700

    added search feature to landing page

--- authenticate-me/frontend/src/components/AllSpots/index.js
+++ authenticate-me/frontend/src/components/AllSpots/index.js
@@ -3,12 +3,15 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getSpotsThunk } from "../../store/spots";
 import './AllSpots.css';
+import { useState } from "react";
 
 const AllSpots = () => {
+    const dispatch = useDispatch();
     const spotsObj = useSelector(state => state.spots.allSpots);
     const spotsArr = Object.values(spotsObj);
+    const spotsArrRev = spotsArr.toReversed();
 
-    const dispatch = useDispatch();
+    const [query, setQuery] = useState("");
 
     //console.log(spotsObj);
 
@@ -16,24 +19,42 @@ const AllSpots = () => {
         dispatch(getSpotsThunk())
     }, [dispatch]);
 
+    //console.log(spotsArrRev);
+
     if (!spotsObj) return null;
 
     return (
         <div>
+            <div className="spot-search-div">
+                <input className="spot-search-bar" placeholder="Search Spots by Location" onChange={e => setQuery(e.target.value)} />
+            </div>
+
             <div className="spots-div">
-                {spotsObj && spotsArr.map(spot => (
-                    <div className="spot-preview" key={spot.id} >
-                        <Link title={spot.name} to={`/spots/${spot.id}`} className="spot-img-div">
-                            <img className="spot-img" src={spot.previewImage} alt="A Spot Pic"></img>
+                {spotsArrRev?.filter(spot => {
+                    if (query === '') {
+                        return spot;
+                    } else if (spot?.city.toLowerCase().includes(query.toLocaleLowerCase())) {
+                        return spot;
+                    } else if (spot?.state.toLowerCase().includes(query.toLocaleLowerCase())) {
+                        return spot;
+                    } else if (spot?.country.toLowerCase().includes(query.toLocaleLowerCase())) {
+                        return spot;
+                    } else if (spot?.name.toLowerCase().includes(query.toLocaleLowerCase())) {
+                        return spot;
+                    }
+                }).map(spot => (
+                    <div className="spot-preview" key={spot?.id} >
+                        <Link title={spot?.name} to={`/spots/${spot?.id}`} className="spot-img-div">
+                            <img className="spot-img" src={spot?.previewImage} alt="A Spot Pic"></img>
                         </Link>
 
                         <div className="details-part-1">
-                            <p>{spot.city}, {spot.state}</p>
+                            <p>{spot?.city}, {spot?.state}</p>
                             
-                            <p><i className="preview-star fas fa-star" />{(spot.avgRating) ? spot.avgRating.toFixed(1) : "New"}</p>
+                            <p><i className="preview-star fas fa-star" />{(spot?.avgRating) ? spot?.avgRating.toFixed(1) : "New"}</p>
                         </div>
                         
-                        <p className="spot-preview-price">${spot.price} Night</p>
+                        <p className="spot-preview-price">${spot?.price} Night</p>
                     </div>
                 ))}
             </div>
